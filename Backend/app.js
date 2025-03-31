8dc863fd95ce47bd6174f81820434502522dfd15
Jo <jordanmoret4@gmail.com>
2025-03-31 22:23:08 +0200

    express-ok

---
+++ Backend/app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Book = require('./models/books');
+
+mongoose.connect('mongodb+srv://jordanProject:YpgZ8dgfL75jJd43@cluster1.g8gke.mongodb.net/',
+    { useNewUrlParser: true,
+      useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.post('/api/stuff', (req, res, next) => {
+    delete req.body._id;
+    const book = new Book({
+      ...req.body,
+    });
+    book.save()
+      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+      .catch(error => res.status(400).json({ error }));
+  });
+
+
+app.get('/api/stuff', (req, res, next) => {
+    Book.find()
+      .then(books => res.status(200).json(books))
+      .catch(error => res.status(400).json({ error }));
+  });
+
+
+
+module.exports = app;
