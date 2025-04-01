172a12839744b9625f5c93d72d6155c9c5ebcb26
Jo <jordanmoret4@gmail.com>
2025-04-01 10:14:55 +0200

    structure

--- Backend/app.js
+++ Backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const Book = require('./models/books');
+const stuffRoutes = require('./routes/stuff');
 
 mongoose.connect('mongodb+srv://jordanProject:AHUsubb5bwqXZuOj@cluster1.g8gke.mongodb.net/',
     { useNewUrlParser: true,
@@ -12,32 +12,17 @@ mongoose.connect('mongodb+srv://jordanProject:AHUsubb5bwqXZuOj@cluster1.g8gke.mo
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json());//??????
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-  });
+  })
 
-app.post('/api/stuff', (req, res, next) => {
-    delete req.body._id;
-    const book = new Book({
-      ...req.body,
-    });
-    book.save()
-      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
+app.use(bodyParser.json());
 
-
-app.get('/api/stuff', (req, res, next) => {
-    Book.find()
-      .then(books => res.status(200).json(books))
-      .catch(error => res.status(400).json({ error }));
-  });
-
-
+app.use('/api/stuff', stuffRoutes);
 
 module.exports = app;
