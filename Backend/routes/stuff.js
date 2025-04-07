5cfbb08a8b702fc140a4cdbadd8fc711af1ec883
Jo <jordanmoret4@gmail.com>
2025-04-07 15:59:53 +0200

    warning-Creabook

--- Backend/routes/stuff.js
+++ Backend/routes/stuff.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const auth = require('../middleware/auth');
 const router = express.Router();
 
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
 const stuffCtrl = require('../controllers/stuff');
 
 router.get('/',auth, stuffCtrl.getAllBooks);
-router.post('/',auth, stuffCtrl.createBook);
+router.post('/',auth, multer, stuffCtrl.createBook);
 router.get('/:id',auth, stuffCtrl.getOneBook);
 router.put('/:id',auth, stuffCtrl.modifyBook);
 router.delete('/:id',auth, stuffCtrl.deleteBook);
