bddb20045a4d0e6b5111f4385a80139e28930a59
Jo <jordanmoret4@gmail.com>
2025-04-07 16:48:02 +0200

    modify-book

--- Backend/routes/stuff.js
+++ Backend/routes/stuff.js
@@ -9,7 +9,7 @@ const stuffCtrl = require('../controllers/stuff');
 router.get('/',auth, stuffCtrl.getAllBooks);
 router.post('/',auth, multer, stuffCtrl.createBook);
 router.get('/:id',auth, stuffCtrl.getOneBook);
-router.put('/:id',auth, stuffCtrl.modifyBook);
+router.put('/:id',auth, multer, stuffCtrl.modifyBook);
 router.delete('/:id',auth, stuffCtrl.deleteBook);
 
 
