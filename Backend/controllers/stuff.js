fd0e9f18b5f8663044bb3a6e8f5e6d8f5efda5f8
Jo <jordanmoret4@gmail.com>
2025-04-14 15:21:09 +0200

    controllers

--- Backend/controllers/stuff.js
+++ Backend/controllers/stuff.js
@@ -3,11 +3,12 @@ const fs = require('fs');
 
 exports.createBook = (req, res, next) => {
     const bookObject = JSON.parse(req.body.book);
-    delete bookObject._id;
-    delete bookObject._userId;
     const book = new Book({
         ...bookObject,
         userId: req.auth.userId,
+        ratings: [],
+        averageRating: 0,
+        //get the image url from the request
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
         });
         book.save()
@@ -16,53 +17,107 @@ exports.createBook = (req, res, next) => {
 };
 
 exports.modifyBook = (req, res, next) => {
+    //is there a new picture ?
     const bookObject = req.file ? {
-        ...JSON.parse(req.body.book),
-        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-      } : { ...req.body };
-
-      delete bookObject._userId;
-      Book.findOne({_id: req.params.id})
-        .then((book) => {
+      //process image if there is one
+      ...JSON.parse(req.body.book),
+      imageUrl: `${req.protocol}://${req.get('host')}/images/opt_${req.file.filename}`
+  } : { ...req.body }; //if not, simply get the data
+  Book.findOne({ _id: req.params.id })
+      .then((book) => {
+          //check user
           if (book.userId != req.auth.userId) {
-            res.status(401).json({ message: 'Non autorisé !' });
+              res.status(401).json({ message: '401: unauthorized request' });
           } else {
-            Book.updateOne({ _id: req.params.id }, { ...bookObject, _id: req.params.id })
-            .then(() => res.status(200).json({ message: 'Objet modifié !' }))
-            .catch(error => res.status(401).json({ error }));
+              //update book corresponding to params id, with the data collected in bookObject
+              Book.updateOne({ _id: req.params.id }, { ...bookObject, _id: req.params.id })
+                  .then(() => {
+                      res.status(200).json({ message: 'book successfully updated' });
+                      //delete old file
+                      const oldFile = book.imageUrl.split('/images')[1];
+                      req.file && fs.unlink(`images/${oldFile}`, (err => {
+                          if (err) console.log(err);
+                      }))
+                  })
+                  .catch(error => res.status(401).json({ error }));
           }
-        })
-        .catch((error) => {
-          res.status(400).json({ error });
-        });
+      })
+      .catch(error => res.status(400).json({ error }));
+
 };
 
+    
+
 exports.deleteBook = (req, res, next) => {
-    Book.findOne({ _id: req.params.id })
-      .then(book => {
-        if (book.userId != req.auth.userId) {
-          res.status(401).json({ message: 'Non autorisé !' });
-        } else {
-          const filename = book.imageUrl.split('/images/')[1];
-          fs.unlink(`images/${filename}`, () => {
-            Book.deleteOne({ _id: req.params.id })
-              .then(() => { res.status(200).json({ message: 'Objet supprimé !' })})
+  Book.findOne({ _id: req.params.id })
+  .then((book) => {
+      //check if user is the owner of the book
+      if (book.userId != req.auth.userId) {
+          res.status(403).json({ message: '403: unauthorized request' });
+      } else {
+          Book.deleteOne({ _id: req.params.id })
+              .then(() => {
+                  res.status(200).json({ message: 'Deleted!' });
+                  //get file name after path
+                  const filename = book.imageUrl.split('/images/')[1];
+                  //unlink from fs package delete the file then execute callback to delete the book in database
+                  fs.unlink(`images/${filename}`, (err => {
+                      if (err) console.log(err);
+                  }))
+              })
               .catch(error => res.status(401).json({ error }));
-           });
-        }
-      })
-      .catch( error =>  { res.status(500).json({ error });
-  });
+      }
+  })
+  .catch(error => res.status(500).json({ error }));
 };
 
 exports.getOneBook = (req, res, next) => {
-    Book.findOne({ _id: req.params.id })
-      .then(book => res.status(200).json(book))
-      .catch(error => res.status(404).json({ error }));
+  Book.findOne({ _id: req.params.id })
+  .then((book) => res.status(200).json(book))
+  .catch(error => res.status(404).json({ error }));
 };
+
 
 exports.getAllBooks = (req, res, next) => {
     Book.find()
-      .then(books => res.status(200).json(books))
+      .then((books) => res.status(200).json(books))
       .catch(error => res.status(400).json({ error }));
 };
+
+exports.ratingBook = (req, res, next) => { 
+  const updatedRating = {
+    userId: req.auth.userId,
+    grade: req.auth.rating
+  };
+  
+  if (updatedRating.grade < 0 || updatedRating.grade > 5) {
+    return res.status(400).json({ message: 'La note doit être comprise entre 0 et 5' });
+  }
+
+  Book.findOne({ _id: req.params.id })
+    .then((book) => {
+      if (book.ratings.find(r => r.userId === req.auth.userId)) {
+        return res.status(400).json({ message: 'Vous avez déjà noté ce livre' });
+      } else { 
+        //push new rating to array
+        book.ratings.push(updatedRating);
+        //calculate average rating
+        book.averageRating = (book.averageRating * (book.ratings.length - 1) + updatedRating.grade) / book.ratings.length;
+        return book.save();
+      }
+    })
+    .then((updatedBook) => res.status(201).json(updatedBook))
+    .catch(error =>  res.status(400).json({ error }));
+}
+
+// get best 3 best rated books 
+exports.getBestRatings = (req, res, next) => {
+  Book.find()
+      //sort by descending order
+      .sort({ averageRating: -1 })
+      //keep the first 3 books (best)
+      .limit(3)
+      //return array of 3 best rated books
+      .then((bestBooks) => res.status(200).json(bestBooks))
+      .catch(error => res.status(400).json({ error }));
+}
--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -1,43 +1,61 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
 const User = require('../models/User');
 
 exports.signup = (req, res, next) => {
+  const { email, password } = req.body;
+
+  // //check email
+  const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  if (emailRegex.test(email) === false) {
+      return res.status(400).json({ message: 'email non valide' });
+  };
+
+  //check password length
+  if (password.length < 5) {
+      return res.status(400).json({ message: 'Le mot de passe doit avoir au minimum 5 caractères' });
+  }
+
+  //hash the password and save new user
   bcrypt.hash(req.body.password, 10)
-    .then(hash => {
-      const user = new User({
-        email: req.body.email,
-        password: hash,
-      });
-      user.save()
-        .then(() => res.status(201).json({ message: 'User crée' }))
-        .catch((error) => res.status(400).json({ error }));
-    })
-    .catch(error => res.status(500).json({ error }));
+      .then(hash => {
+          const user = new User({
+              email: req.body.email,
+              password: hash
+          });
+          user.save()
+              .then(() => res.status(201).json({ message: 'utilisateur créé' }))
+              .catch(error => res.status(400).json({ error }));
+      })
+      .catch(error => res.status(500).json({ error }));
 };
 
 exports.login = (req, res, next) => {
   User.findOne({ email: req.body.email })
-    .then(user => {
-      if (!user) {
-        return res.status(401).json({ error: 'Paire identifiant/mot de passe incorrect' });
-      }
-        bcrypt.compare(req.body.password, user.password)
-          .then(valid => {
-            if (!valid) {
-              return res.status(401).json({ error: 'Paire identifiant/mot de passe incorrect' });
-            } 
-            
-            res.status(200).json({
-                userId: user._id,
-                token: jwt.sign(
-                  {userId: user._id},
-                  'Random_secret_key',
-                  { expiresIn: '24h' }
-                )    
-              });
-            })         
-          .catch(error => res.status(500).json({ error }));
-          })
-          .catch(error => res.status(500).json({ error }));
+      .then(user => {
+          if (user === null) {
+              res.status(401).json({ message: 'Les informations de connection sont incorrectes.' })
+          } else {
+              bcrypt.compare(req.body.password, user.password)
+                  .then(valid => {
+                      if (!valid) {
+                          res.status(401).json({ message: 'Les informations de connection sont incorrectes.' })
+                      } else {
+                          res.status(200).json({
+                              userId: user._id,
+                              token: jwt.sign(
+                                  { userId: user._id },
+                                  process.env.TOKEN,
+                                  { expiresIn: '24h' },
+                              )
+                          });
+                      }
+                  })
+                  .catch(error => res.status(500).json({ error }));
+          }
+      })
+      .catch(error => res.status(500).json({ error }));
+
 };
