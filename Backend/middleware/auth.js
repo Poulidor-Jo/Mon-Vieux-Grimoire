77fe5b329903cf964ba62d65ab76edb60b153da5
Jo <jordanmoret4@gmail.com>
2025-04-15 10:21:35 +0200

    add.proccess.env

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 module.exports = (req, res, next) => {
     try{
         const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = jwt.verify(token, 'Random_secret_key');
+        const decodedToken = jwt.verify(token, process.env.Random_secret_key);
         const userId = decodedToken.userId;
         req.auth = {
             userId: userId
