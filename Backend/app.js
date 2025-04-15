f40950bff961b65e09d837cd3a50b4f04cfebbc4
Jo <jordanmoret4@gmail.com>
2025-04-15 11:59:13 +0200

    supp-comment-limiter

--- Backend/app.js
+++ Backend/app.js
@@ -15,10 +15,10 @@ const app = express();
 
 //rate limit prevents brute force attacks on all app
 const limiter = rateLimit({
-  windowMs: 10 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  windowMs: 10 * 60 * 1000,
+  max: 100, 
+  standardHeaders: true, 
+  legacyHeaders: false, 
 })
 
 //CORS
